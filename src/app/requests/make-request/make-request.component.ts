e2e84cf6722c404c0920f7dc7246dd69e0b79406
Yindoom <31922816+Yindoom@users.noreply.github.com>
2018-12-06 15:31:22 +0100

    Data requests table shrug

--- src/app/requests/make-request/make-request.component.ts
+++ src/app/requests/make-request/make-request.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {RequestService} from '../../Shared/Services/request.service';
 import {Sample} from '../../Shared/Models/sample';
-import {MatTableModule} from '@angular/material';
 
 
 @Component({
@@ -23,14 +22,15 @@ export class MakeRequestComponent implements OnInit {
   samples: Sample[];
 
   typeoptions = ['Average', 'Point', 'Maximum', 'Minimum'];
-  displayedColumns: string[] = ['timestamp', 'quality', 'value'];
+
+  displayedColumns = ['timestamp', 'quality', 'value'];
+
   constructor(private service: RequestService) { }
 
   ngOnInit() {
 
   }
   save() {
-    debugger;
     const req = this.sendRequestForm.value;
     if (req.sampleType === '') {
       req.sampleType = 'Average';
